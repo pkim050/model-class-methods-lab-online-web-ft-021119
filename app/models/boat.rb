0f4e7047232ec7d75f4403ed593651a88b52e5a6
Patrick Kim <pkim050@gmail.com>
2019-05-13 17:15:20 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,7 +4,7 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    Boat.all[0 ... 4]
+    Boat.all[0..4]
   end
 
   def self.dinghy
