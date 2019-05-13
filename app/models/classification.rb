37d2fb167da2ce20c178490044e5245b184ec881
Patrick Kim <pkim050@gmail.com>
2019-05-13 17:28:59 +0000

    Automatically backed up by Learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -3,10 +3,11 @@ class Classification < ActiveRecord::Base
   has_many :boats, through: :boat_classifications
 
   def self.my_all
-
+    Classification.all
   end
 
   def self.longest
-
+    longest = Boat.order(length: :desc).first
+    longest.classifications
   end
 end
