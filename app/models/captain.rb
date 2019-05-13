977c285db23c9a30f07a3189881636c7d444d015
Patrick Kim <pkim050@gmail.com>
2019-05-13 17:26:35 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,11 +2,13 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-
+    catamaran = Classification.find_by(name: "Catamaran")
+    catamaran.boats.collect{|boat| boat.captain}
   end
 
   def self.sailors
-
+    sailboat = Classification.find_by(name: "Sailboat")
+    sailboat.boats.collect{|boat| boat.captain}
   end
 
   def self.talented_seafarers
@@ -14,6 +16,6 @@ class Captain < ActiveRecord::Base
   end
 
   def self.non_sailors
-
+    Captain.all - Captain.sailors
   end
 end
