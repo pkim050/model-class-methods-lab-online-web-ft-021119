a9657aa7ad00d745fd977b642014ebf271698ff5
Patrick Kim <pkim050@gmail.com>
2019-05-13 17:41:31 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,33 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    Boat.all[0..4]
+  end
+
+  def self.dinghy
+    Boat.select{|boat| boat.length < 20}
+  end
+
+  def self.ship
+    Boat.select{|boat| boat.length >= 20}
+  end
+
+  def self.last_three_alphabetically
+    Boat.order(name: :desc).first(3)
+  end
+
+  def self.without_a_captain
+    Boat.select{|boat| boat.captain_id == nil}
+  end
+
+  def self.sailboats
+    sailboat = Classification.find_by(name: "Sailboat")
+    sailboat.boats
+  end
+
+  def self.with_three_classifications
+    Boat.select{|boat| boat.classifications.count == 3}
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,26 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    catamaran = Classification.find_by(name: "Catamaran")
+    catamaran.boats.collect{|boat| boat.captain}
+  end
+
+  def self.sailors
+    sailboat = Classification.find_by(name: "Sailboat")
+    #binding.pry
+    sailboat.boats.collect{|boat| boat.captain}.compact.uniq
+  end
+
+  def self.talented_seafarers
+    motorboat = Classification.find_by(name: "Motorboat")
+    motor_cap = motorboat.boats.collect{|boat| boat.captain}.compact.uniq
+    sailboat = Classification.find_by(name: "Sailboat")
+    sail_cap = sailboat.boats.collect{|boat| boat.captain}.compact.uniq
+    motor_cap & sail_cap
+  end
+
+  def self.non_sailors
+    Captain.all - Captain.sailors
+  end
 end
